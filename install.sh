85f151ad5c795bd9153f2b8a196d1cfa690afa6d
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-08-16 23:29:10 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s $(pwd)/i3 $HOME/.config/i3
+ln -s $(pwd)/nvim $HOME/.config/nvim
+
+ln -s $(pwd)/.bashrc $HOME/.bashrc
+ln -s $(pwd)/.bash_profile $HOME/.bash_profile
+ln -s $(pwd)/.xinitrc $HOME/.xinitrc
+ln -s $(pwd)/.Xresources $HOME/.Xresources
