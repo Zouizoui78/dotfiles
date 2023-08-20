28b7025e8db2a82882ba2c8f64704e274bdb05b6
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-08-20 10:26:36 +0200

    lsp clangd : don't auto-include

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,7 +6,10 @@ lsp.on_attach(function(_, bufnr)
     lsp.default_keymaps({buffer = bufnr})
 end)
 
-require('lspconfig').clangd.setup{}
+require("lspconfig").clangd.setup {
+    cmd = { "clangd", "--header-insertion=never" }
+}
+
 require('lspconfig').cmake.setup{}
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
